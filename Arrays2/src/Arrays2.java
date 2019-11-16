a91dcb1cb833a814f4369dba28e02e6b8490e44c
trofimoff <44056222+chackydude@users.noreply.github.com>
2019-11-17 02:06:52 +0300

    my mate's sorting sorting algorithm

--- Arrays2/src/Arrays2.java
+++ Arrays2/src/Arrays2.java
@@ -4,11 +4,11 @@ public class Arrays2 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the quantity");
-        int a=Integer.parseInt(sc.nextLine());
-        int[] array=new int[a];
-        for(int i=0;i<array.length;i++){
+        int a = Integer.parseInt(sc.nextLine());
+        int[] array = new int[a];
+        for(int i = 0; i < array.length; i++){
             System.out.print("Enter the num: ");
-            array[i]=sc.nextInt();
+            array[i] = sc.nextInt();
         }
         insert(array);
         System.out.println(Arrays.toString(array));
@@ -16,12 +16,12 @@ public class Arrays2 {
     public static void insert(int[] array){
         int k;
         int p;
-        for(int i=0;i<array.length-1;i++){
+        for(int i = 0; i < array.length-1; i++){
             k=i;
-            while(k >= 0 && array[k]>array[k+1]){
+            while(k >= 0 && array[k] > array[k+1]){
                 p=array[k+1];
-                array[k+1]=array[k];
-                array[k]=p;
+                array[k+1] = array[k];
+                array[k] = p;
                 k--;
             }
         }
